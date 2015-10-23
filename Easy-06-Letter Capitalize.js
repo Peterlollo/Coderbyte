343d2dc9d20965868464a19973baa3b3ab4d8fbe
Peter Lollo <peteralollo@gmail.com>
2015-10-23 16:40:38 -0700

    Create Easy-06-Letter Capitalize.js

---
+++ Easy-06-Letter Capitalize.js
@@ -0,0 +1,17 @@
+function LetterCapitalize(str) { 
+  var answer = "";
+  
+for(i=0;i<str.length;i++) {
+ if(i === 0) {
+   answer += str[i].toUpperCase();
+ } 
+ else if(str[i-1] === " ") {
+   answer += str[i].toUpperCase();
+ } 
+ else {
+   answer += str[i];
+ }
+}
+
+  return answer;
+}
