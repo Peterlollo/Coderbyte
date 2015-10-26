73d4462e7819879421a54f1810959c26de9c98bf
Peter Lollo <peteralollo@gmail.com>
2015-10-26 09:33:44 -0700

    Update Easy-16-Arith Geo.js

--- Easy-16-Arith Geo.js
+++ Easy-16-Arith Geo.js
@@ -25,6 +25,44 @@ function ArithGeo(arr) {
       answer = "Geometric";
     }
 
+
+// Alternate Solution
+
+function arith(arr) {
+   var diff = arr[1] - arr[0];
+  for(i=0; i<arr.length-1; i++) {
+  if(arr[i+1] - diff !== arr[i]) {
+    return false;
+}
+  }
+    return true;
+  }
+    
+    function geo(arr) {
+        var divisor = arr[1]/arr[0];
+  for(i=0; i<arr.length-1; i++) {
+    if (arr[i+1]/divisor !== arr[i]) {
+    return false;
+  }
+  }
+    return true;
+    }
+    
+    
+    
+function ArithGeo(arr) { 
+  if(arr.length<3) {
+    return -1;
+  }
+  else if(arith(arr)) {
+    return "Arithmetic";
+} else if (geo(arr)) {
+  return "Geometric";
+}
+  else {
+    return -1;
+  }
+}
   return answer;
          
 }
