27319840858c203d3e3937300702e1d47d0428b7
Peter Lollo <peteralollo@gmail.com>
2015-12-14 16:53:39 -0800

    Update Easy-02-First Factorial.js

--- Easy-02-First Factorial.js
+++ Easy-02-First Factorial.js
@@ -1,3 +1,16 @@
+//With Recursion
+function FirstFactorial(num) { 
+if(num < 0) {
+  return -1;
+}
+  else if(num === 0) {
+    return 1;
+  }
+  return (num * FirstFactorial(num - 1));    
+}
+
+
+//Without Recursion
 function FirstFactorial(num) { 
 var answer = 1;
 for(i=num;i>0;i--) {
