451a1963cb0df7d794ab43e0936b16aaa3aacd04
Peter Lollo <peteralollo@gmail.com>
2015-12-29 11:15:22 -0800

    With Recursion

--- Easy-07-Simple Symbols.js
+++ Easy-07-Simple Symbols.js
@@ -1,3 +1,26 @@
+//With Recursion
+function SimpleSymbols(str) { 
+  if(str.length <= 0) {
+    return true;
+  }
+  else if(!(str.match(/[a-z]/i))) {
+    return true;
+  }
+  str = str.split('');
+  for(var i = 0; i<str.length; i++) {
+    if(str[i].match(/[a-z]/i)) {
+      if(str[i-1] !== '+' || str[i+1] !== '+') {
+        return false;
+      }
+      str = str.join('');
+      str = str.slice(i+1);
+      return SimpleSymbols(str);
+    }
+  }
+}
+
+
+//Without Recursion
 function SimpleSymbols(str) { 
 
   for(i=0;i<str.length;i++) {
