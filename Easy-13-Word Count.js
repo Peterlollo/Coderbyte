c395cd51e3d1c1b42abadb38e3076211a73fcd98
Peter Lollo <peteralollo@gmail.com>
2015-12-30 11:07:36 -0800

    Update Easy-13-Word Count.js

--- Easy-13-Word Count.js
+++ Easy-13-Word Count.js
@@ -1,3 +1,17 @@
+//With Recursion
+var wordCount = function (sen) {
+  if(sen.length === 0) {
+    return 0;
+  }
+  sen = sen.split(' ');
+  var count = 0;
+  sen.pop();
+  count++;
+  sen = sen.join(' ');
+  return count += wordCount(sen);
+}
+
+//Without Recursion
 function WordCount(str) { 
 return str.split(" ").length;
         
